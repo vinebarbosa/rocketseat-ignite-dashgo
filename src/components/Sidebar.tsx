c8ee188b000dfd967fa058b1bbc3d767f4f5ad94
mateusdeitos <mate.deitos@hotmail.com>
2021-04-12 22:56:07 -0300

    Dashboard

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import Icon from "@chakra-ui/icon"
+import { Box, Stack, Text } from "@chakra-ui/layout"
+import { Link } from "@chakra-ui/react"
+import React from "react"
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri"
+
+export const Sidebar = () => {
+    return (
+        <Box as="aside" w="64" mr="8">
+            <Stack spacing="12" align="flex-start">
+                <Box>
+                    <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
+                    <Stack spacing="4" mt="8" align="stretch">
+                        <Link d="flex" align="center">
+                            <Icon as={RiDashboardLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Dashboard</Text>
+                        </Link>
+                        <Link d="flex" align="center">
+                            <Icon as={RiContactsLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Usuários</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+                <Box>
+                    <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
+                    <Stack spacing="4" mt="8" align="stretch">
+                        <Link d="flex" align="center">
+                            <Icon as={RiInputMethodLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Formulários</Text>
+                        </Link>
+                        <Link d="flex" align="center">
+                            <Icon as={RiGitMergeLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Automação</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
+    )
+}
